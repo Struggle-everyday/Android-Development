25733ae700036b2ce15df326f5f37f8a93abcdba
赵艺博 <1164171916@qq.com>
2021-12-28 23:28:29 +0800

    UI和其他相关Java文件上传

---
+++ app/src/main/java/com/example/yantu/Help.java
@@ -0,0 +1,15 @@
+package com.example.yantu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
